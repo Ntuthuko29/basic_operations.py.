c5de5aab622194708bead5bf95d4e47eb2db6830
Ntuthuko29 <giftntuthuko04@gmail.com>
2025-07-14 19:49:13 +0200

    introduction to python
    Basic Arithmetic exercise

---
+++ basic_operations.py.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print("The sum of", number1 ,"and",number2,"is:",sum_result)
+print("The difference of" , number1,"and" ,number2,"is:", difference_result )
+print("the product of" , number1 , "and", number2, "is:", product_result)
